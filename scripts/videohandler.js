02314bdbb2ab4656d87eb79d1b5f491d6cce4c4b
Luke Pulverenti <luke.pulverenti@gmail.com>
2018-06-30 14:58:49 -0400

    update videohandler.js

--- scripts/videohandler.js
+++ scripts/videohandler.js
@@ -1,8 +1,11 @@
 require(['playbackManager', 'events'], function (playbackManager, events) {
     'use strict';
 
+    var videoOn;
+
     events.on(playbackManager, "playbackstart", function (e, player) {
-        if (player.requiresVideoTransparency && playbackManager.isPlayingVideo()) {
+        if (playbackManager.isPlayingVideo()) {
+            videoOn = true;
             var xhr = new XMLHttpRequest();
             xhr.open("POST", "electronapphost://video-on", true);
             xhr.send();
@@ -10,10 +13,21 @@ require(['playbackManager', 'events'], function (playbackManager, events) {
     });
     events.on(playbackManager, "playbackstop", function (e, stopInfo) {
         var player = stopInfo.player;
-        if (player.requiresVideoTransparency) {
+        if (videoOn) {
+            videoOn = false;
             var xhr = new XMLHttpRequest();
             xhr.open("POST", "electronapphost://video-off", true);
             xhr.send();
         }
     });
+
+    function sendCommand(name) {
+
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'electronapphost://' + name, true);
+
+        xhr.send();
+    }
+
+    sendCommand('loaded');
 });
